da2f2aca17827604d6edd84f01772e09a3c1a359
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-12 07:15:22 +0800

    Removed obsolete methods

--- src/CodeCompanion.EntityFrameworkCore/CodeCompanionDbContext.cs
+++ src/CodeCompanion.EntityFrameworkCore/CodeCompanionDbContext.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,46 +23,6 @@ namespace CodeCompanion.EntityFrameworkCore
         public void EnableHotSave() => isHotSaveEnabled = true;
 
         public void DisableHotSave() => isHotSaveEnabled = false;
-
-        [Obsolete("Use 'TrySaveChanges(...)' or 'TrySaveChangesAsync(...)' instead")]
-        public virtual int HotSaveChanges() => isHotSaveEnabled
-            ? SaveChanges()
-            : throw new HotSaveDisabledException();
-        
-        [Obsolete("Use 'TrySaveChanges(...)' or 'TrySaveChangesAsync(...)' instead")]
-        public virtual int HotSaveChanges(bool acceptAllChangesOnSuccess) => isHotSaveEnabled
-            ? SaveChanges(acceptAllChangesOnSuccess)
-            : throw new HotSaveDisabledException();
-
-        [Obsolete("Use 'TrySaveChangesAsync(...) instead'")]
-        public virtual async Task<int> HotSaveChangesAsync(CancellationToken cancellationToken = default) => isHotSaveEnabled
-            ? await SaveChangesAsync(cancellationToken)
-            : throw new HotSaveDisabledException();
-
-        [Obsolete("Use 'TrySaveChangesAsync(...) instead'")]
-        public virtual async Task<int> HotSaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default) => isHotSaveEnabled
-            ? await SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken)
-            : throw new HotSaveDisabledException();
-
-        [Obsolete("Use 'TrySaveChanges(...)' or 'TrySaveChangesAsync(...)' instead")]
-        public virtual int? TryHotSaveChanges() => isHotSaveEnabled
-            ? SaveChanges()
-            : null;
-        
-        [Obsolete("Use 'TrySaveChanges(...)' or 'TrySaveChangesAsync(...)' instead")]
-        public virtual int? TryHotSaveChanges(bool acceptAllChangesOnSuccess) => isHotSaveEnabled
-            ? SaveChanges()
-            : null;
-
-        [Obsolete("Use 'TrySaveChangesAsync(...) instead'")]
-        public virtual async Task<int?> TryHotSaveChangesAsync(CancellationToken cancellationToken = default) => isHotSaveEnabled
-            ? await SaveChangesAsync(cancellationToken)
-            : null;
-
-        [Obsolete("Use 'TrySaveChangesAsync(...) instead'")]
-        public virtual async Task<int?> TryHotSaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default) => isHotSaveEnabled
-            ? await SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken)
-            : null;
 
         public virtual int? TrySaveChanges() => isHotSaveEnabled ? SaveChanges() : null;
 
