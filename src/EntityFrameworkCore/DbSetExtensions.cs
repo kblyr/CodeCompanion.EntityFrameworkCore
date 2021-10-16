883848dbead86ec8a6d253f06ffef9fcc8a479d0
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-17 00:49:58 +0800

    Find by ID FindById(id) & FindByIdAsync(id, cancellationToken)

--- src/EntityFrameworkCore/DbSetExtensions.cs
+++ src/EntityFrameworkCore/DbSetExtensions.cs
@@ -20,5 +20,23 @@ namespace CodeCompanion.EntityFrameworkCore
         public static EntityEntry<TEntity> SoftDelete<TEntity>(this DbSet<TEntity> entities, TEntity entity, Footprint footprint) where TEntity : class => entities
             .Update(entity)
             .AsSoftDeleted(footprint);
+
+        public static TEntity? FindById<TEntity>(this DbSet<TEntity> entities, short id) where TEntity : class => entities
+            .Find(new object[] { id });
+
+        public static TEntity? FindById<TEntity>(this DbSet<TEntity> entities, int id) where TEntity : class => entities
+            .Find(new object[] { id });
+
+        public static TEntity? FindById<TEntity>(this DbSet<TEntity> entities, long id) where TEntity : class => entities
+            .Find(new object[] { id });
+
+        public static async Task<TEntity?> FindByIdAsync<TEntity>(this DbSet<TEntity> entities, short id, CancellationToken cancellationToken = default) where TEntity : class => await entities
+            .FindAsync(new object[] { id }, cancellationToken);
+
+        public static async Task<TEntity?> FindByIdAsync<TEntity>(this DbSet<TEntity> entities, int id, CancellationToken cancellationToken = default) where TEntity : class => await entities
+            .FindAsync(new object[] { id }, cancellationToken);
+
+        public static async Task<TEntity?> FindByIdAsync<TEntity>(this DbSet<TEntity> entities, long id, CancellationToken cancellationToken = default) where TEntity : class => await entities
+            .FindAsync(new object[] { id }, cancellationToken);
     }
 }
